f410d39e54fc7710991ba7723a3b6058ccb70442
gianveloxbr <gianveloxsi@gmail.com>
2019-04-09 10:50:54 -0300

    Aula 09/04

---
+++ consultaEmpresa.js
@@ -0,0 +1,25 @@
+db.funcionarios.aggregate([{"$match":{"Empresa":"Google"}},
+                            {"$group":{"_id":"$Empresa",
+                                "total":{"$sum":1}}}
+                        ])
+                                
+db.funcionarios.aggregate([{"$match":{"Empresa":"Google"}},
+                            {"$count":"total"}
+                        ])
+                            
+db.funcionarios.aggregate([{"$match":{"Empresa":"Google"}},
+                            {"$project":{
+                                "Nome":1,
+                                "Cargo":1,
+                                _id:0
+                            }
+                        }])
+                        
+db.funcionarios.aggregate([{"$match":{"Empresa":"Google"}},
+                            {"$project":{
+                                "Nome":1,
+                                "Ocupação":"$Cargo",
+                                _id:0
+                            }
+                        }])
+
---
+++ consultaPokemon.js
@@ -0,0 +1,13 @@
+db.combats.find()
+
+db.pokemons.find({HP: {$gte:40}},
+                  {Name: 1,
+                   HP: 1,
+                   _id:0}).sort({Name:1}).skip(100).limit(100)
+                   
+var items = db.pokemons.find({HP: {$gte:40}},
+                  {Name: 1,
+                   HP: 1,
+                   _id:0}).sort({Name:1}).skip(100).limit(100)
+                   
+items
